06f85be2f67074ab83b213bb546f0fa963692068
xinchen.luan <38126800+luanxinchen@users.noreply.github.com>
2024-06-19 17:45:39 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.newmorning.work": dockerHub,
+  "quay.newmorning.work": "https://quay.io",
+  "gcr.newmorning.work": "https://gcr.io",
+  "k8s-gcr.newmorning.work": "https://k8s.gcr.io",
+  "k8s.newmorning.work": "https://registry.k8s.io",
+  "ghcr.newmorning.work": "https://ghcr.io",
+  "cloudsmith.newmorning.work": "https://docker.cloudsmith.io",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
